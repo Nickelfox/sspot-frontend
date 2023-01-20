9bda2c36d7c8e0e1ea61ac270b32c98a11abd25b
Ravi K <ravi.maurya@nickelfox.com>
2023-01-20 17:09:19 +0530

    chore: setup sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import * as Sentry from "@sentry/react"
+import { BrowserTracing } from "@sentry/tracing"
+
+Sentry.init({
+  dsn: "https://966db2db179c4c8ca45de91d49b08b34@o880021.ingest.sentry.io/4504536592285696",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
