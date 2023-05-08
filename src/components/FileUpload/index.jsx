6f6174ed95708cb6a0af26168e87ff446e74a908
Ankita_Nickelfox <ankita.daundkar@nickelfox.com>
2023-05-08 15:20:38 +0530

    refactor(FileUpload): Prettier Apply

--- src/components/FileUpload/index.jsx
+++ src/components/FileUpload/index.jsx
@@ -1,15 +1,11 @@
-import React from "react";
-import { Box } from "@mui/material";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import CloseIcon from "@mui/icons-material/Close";
-import { useFileController } from "./file.controller";
-import styles from "./FileUpload.module.css";
+import React from "react"
+import { Box } from "@mui/material"
+import CloudUploadIcon from "@mui/icons-material/CloudUpload"
+import CloseIcon from "@mui/icons-material/Close"
+import { useFileController } from "./file.controller"
+import styles from "./FileUpload.module.css"
 
-function FileUpload({
-  label = "Browse Files",
-  max_files = 5,
-  imagePreview = false,
-}) {
+function FileUpload({ label = "Browse Files", max_files = 5, imagePreview = false }) {
   const {
     handleFileEvent,
     handleCloseFile,
@@ -20,8 +16,8 @@ function FileUpload({
     isDraggingOver,
     handleDragOverEvent,
     handleDragLeaveEvent,
-    getFileIcon,
-  } = useFileController(max_files);
+    getFileIcon
+  } = useFileController(max_files)
 
   return (
     <Box className={styles.file_container}>
@@ -41,8 +37,7 @@ function FileUpload({
         onClick={() => document.getElementById("contained-button-file").click()}
         onDrop={handleDropEvent}
         onDragOver={handleDragOverEvent}
-        onDragLeave={handleDragLeaveEvent}
-      >
+        onDragLeave={handleDragLeaveEvent}>
         <CloudUploadIcon />
         <Box>
           <label className={styles.cursor_pointer}>{label}</label>
@@ -61,16 +56,13 @@ function FileUpload({
                   )}
                   <span>{limitFileName(file.name, 25)}</span>
                 </Box>
-                <CloseIcon
-                  className={styles.cursor_pointer}
-                  onClick={() => handleCloseFile(idx)}
-                />
+                <CloseIcon className={styles.cursor_pointer} onClick={() => handleCloseFile(idx)} />
               </Box>
-            );
+            )
           })}
       </Box>
     </Box>
-  );
+  )
 }
 
-export default FileUpload;
+export default FileUpload
