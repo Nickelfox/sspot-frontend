1b68ec7fd1641c786aa6ab0839dad4f440a8d6d5
Ankita_Nickelfox <ankita.daundkar@nickelfox.com>
2023-05-08 13:11:42 +0530

    refactor(Component): Prettier Apply

--- src/components/FileUpload/FileUpload.model.js
+++ src/components/FileUpload/FileUpload.model.js
@@ -1,30 +1,24 @@
 export const useUploadFileModel = () => {
-  const handleUploadFiles = (
-    files,
-    uploadedFiles,
-    max_files,
-    setFileLimit,
-    setUploadedFiles
-  ) => {
-    const uploaded = [...uploadedFiles];
-    let limitExceeded = false;
+  const handleUploadFiles = (files, uploadedFiles, max_files, setFileLimit, setUploadedFiles) => {
+    const uploaded = [...uploadedFiles]
+    let limitExceeded = false
 
     files.some((file) => {
       if (uploaded.findIndex((f) => f.name === file.name) === -1) {
-        uploaded.push(file);
+        uploaded.push(file)
 
-        if (uploaded.length === max_files) setFileLimit(true);
+        if (uploaded.length === max_files) setFileLimit(true)
         if (uploaded.length > max_files) {
-          alert(`You can only add a maximum of ${max_files} files`);
-          setFileLimit(false);
-          limitExceeded = true;
-          return true;
+          alert(`You can only add a maximum of ${max_files} files`)
+          setFileLimit(false)
+          limitExceeded = true
+          return true
         }
       }
-    });
-    if (!limitExceeded) setUploadedFiles(uploaded);
-  };
+    })
+    if (!limitExceeded) setUploadedFiles(uploaded)
+  }
   return {
-    handleUploadFiles,
-  };
-};
+    handleUploadFiles
+  }
+}
