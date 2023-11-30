3e0f6d8c59215e2c80e0ee4c47c967c36451d9c3
Mehran-Nickelfox <mehran.siddiqui@nickelfox.com>
2023-11-30 16:26:13 +0530

    fix(imports): Login Import

--- src/router/routes/publicRoutes.js
+++ src/router/routes/publicRoutes.js
@@ -2,10 +2,9 @@
 import React from "react"
 
 const ForgotPassword = React.lazy(() => import("pages/public/forgot-password"))
-const Login = React.lazy(() => import("pages/public/Login"))
 const SignUp = React.lazy(() => import("pages/public/signup"))
 const ResetPassword = React.lazy(() => import("pages/public/reset-password"))
-
+const Login = React.lazy(() => import("pages/public/Login"))
 export const PublicRoutes = [
   { path: "/auth/login", exact: true, component: Login },
   { path: "/auth/signup", exact: true, component: SignUp },
