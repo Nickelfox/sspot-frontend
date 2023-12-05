ec58d0395a13b1f640de38c4eb6618bf5c770b06
Mehran-Nickelfox <mehran.siddiqui@nickelfox.com>
2023-12-05 15:11:48 +0530

    feat(loginAPI): Integrate Login API

--- src/pages/public/LoginX/login.model.js
+++ src/pages/public/LoginX/login.model.js
@@ -1,17 +1,29 @@
-// import { useUserSession } from "hooks/userSession"
+import { useUserSession } from "hooks/userSession"
 import { NetworkManager, API } from "network/core"
+import { UserState } from "redux/dispatcher/UserState"
 // import { useDispatch } from "react-redux"
 export const useLoginModel = () => {
-  // const userSession = useUserSession()
+  const userSession = useUserSession()
   // const dispatch = useDispatch()
   const loginByEmail = async (values) => {
     const instance = NetworkManager(API.AUTH.LOGIN)
     const response = await instance.request(values)
-    console.log(response, "LoginResponse")
-    // if (response.success) {
-    //   userSession.setSession(response.data)
-    // }
-    // return response.success
+    if (!response?.success) {
+      userSession.setSession(response.data)
+      const reduxObject = {
+        name: response?.data?.user?.full_name,
+        id: response?.data?.user?.id,
+        email: response?.data?.user?.email,
+        phone: response?.data?.user?.phone_number,
+        role: response?.data?.user?.role,
+        designation: response?.data?.user?.designation,
+        company_id: response?.data?.user?.company_id,
+        company_name: response?.data?.user?.company_name,
+        company_logo: response?.data?.user?.company_lo
+      }
+      UserState.login(reduxObject)
+    }
+    return response?.success
   }
 
   return {
